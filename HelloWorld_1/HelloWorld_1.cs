a1b28276943468df6df0d36752f04153bb8ee417
beano <beano@LAPTOP-F9LU9R59>
2023-09-27 14:04:39 +1000

    Add project files.

---
+++ HelloWorld_1/HelloWorld_1.cs
@@ -0,0 +1,23 @@
+namespace HelloWorld_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+		engine.GenerateInformation("Hello Empower!")
+		}
+	}
+}
